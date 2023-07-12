fbd9e0d408a0ba27c9d2ebf1c1be7761f9b190ae
jannatul_axsa <jannatulaxajanifa586@gmail.com>
2023-07-13 04:01:33 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,54 @@
+// ১. Variable কি ?
+// ans: variable is a "specific computer memory storage" where are we can storage & use different type of value.
+
+// ২. Variable কিভাবে লিখতে হয় ?
+// ans: var
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+// ans: string type variable a latter use kora hoy hoy. ata jkono word/name/latter/blog etc kaje use kora hoy. Ex: var myName ="Jannatul Axsa";
+
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+// ans: number type variable a number use kora hoy hoy. Ex: var numType =30;
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+// ans: boolean type variable a 0/1 kaj kore. true/false(reserve keyword). 
+//Ex: var boolType =true;
+// console.log(boolType)
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+// ans: var loveLatter = "jdhfo cjhds udhfsdj jhdsh ihfdjhf udhfjh udhfsjdhb hn jhjhc hjh ";
+// console.log(loveLatter.toUpperCase())
+
+// var loveLatter = "jdhfo cjhds udhfsdj jhdsh ihfdjhf udhfjh udhfsjdhb hn jhjhc hjh ";
+// console.log(loveLatter.toLowerCase())
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+// ans: 5 oparetor. +,-,*,/,%
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন । 
+// ans: var voter = -0.45;
+// var sum = Math.ceil(voter);
+// console.log(sum)-------0.45
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+// ans: var voter = 0.45;
+// var sum = Math.ceil(voter);
+// console.log(sum)-------1
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+// ans: var voter = 0.55;
+// var sum = Math.round(voter);
+// console.log(sum)-------0
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+// ans: var voter = 0.55;
+// var sum = Math.round(voter);
+// console.log(sum)------1
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+// ans: var sum = Math.random()*15
+// console.log(sum)
+
+// asignment.js - GitHub Link 
+// ans: var numType =30;
